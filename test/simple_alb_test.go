4b33929ab1c0c71461ef75c27d4814c78574efa3
amandasoaares <amanda14_sc@hotmail.com>
2020-04-29 18:15:59 -0300

    create script test go

---
+++ test/simple_alb_test.go
@@ -0,0 +1,37 @@
+package test
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/random"
+	"github.com/gruntwork-io/terratest/modules/terraform"
+)
+
+func TestSimpleAlb(t *testing.T) {
+	t.Parallel()
+
+	// Expected values
+	// expVpcCidr := "10.120.0.0/16"
+	expectedName := fmt.Sprintf("vpc-terratest-%s", strings.ToLower(random.UniqueId()))
+	// awsRegion := aws.GetRandomStableRegion(t, nil, nil)
+
+	terraformOptions := &terraform.Options{
+		TerraformDir: "../examples/simple-alb",
+		Upgrade:      true,
+
+		Vars: map[string]interface{}{
+			"vpc_name": expectedName,
+		},
+	}
+	// At the end of the test, run `terraform destroy` to clean up any resources that were created
+	defer terraform.Destroy(t, terraformOptions)
+
+	// This will run `terraform init` and `terraform apply` and fail the test if there are any errors
+	terraform.InitAndApply(t, terraformOptions)
+
+	// vpcCidr := terraform.Output(t, terraformOptions, "vpc_cidr_block")
+	// assert.Equal(t, vpcCidr, expVpcCidr)
+
+}
